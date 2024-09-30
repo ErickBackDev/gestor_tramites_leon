d2dff0b729856b0f630d5f4285be8b4f3bbc57ce
ErickBackDev <erickson17graterol@gmail.com>
2024-09-29 21:33:06 -0400

    establishing connection with the database in production

--- procesos/conexion.php
+++ procesos/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conectar=mysqli_connect('127.0.0.1','root','','gestor_leon');
+	$conectar=mysqli_connect('benpb4ui9luo7mm7cwuw-mysql.services.clever-cloud.com','ukqxmoatp1jqy6ge','ID9mnJQf51EwoXfZ5qbS','benpb4ui9luo7mm7cwuw');
 	// esta linea de abajo es para q la base de datos reconosca los caracteres especiales
 	$conectar->set_charset('utf8mb4');
 	if(!$conectar){
