3788c2a354cba3c7ac4234824dc0f3a4c1ea0a1a
ErickBackDev <erickson17graterol@gmail.com>
2024-10-06 18:06:49 -0400

    fix text position on main page

--- vistas/inicio.php
+++ vistas/inicio.php
@@ -33,48 +33,46 @@
         <button class="tablinks" onclick="mostrarApoyanos()">Apoyanos</button>
     </div>
 
-    <div class="container">
-    	<div class="row">
-    		<div class="col">
-    			<h1>
-    				<p style="text-align: center;">Sociedad Genealógica</p>
-    				<p style="text-align: center;">"El León De La Cordillera".</p>
-    			</h1><br>
+	<div class="row mx-5">
+		<div class="col">
+			<h1>
+				<p style="text-align: center;">Sociedad Genealógica</p>
+				<p style="text-align: center;">"El León De La Cordillera".</p>
+			</h1><br>
 
-    			<p>
-    				<div id="finalidad">
-    					<div class="row">
-    						<div class="col-sm-1"></div>
-    						<div class="col-sm-11">
-    							<p style="text-align: justify;">Ya que gran parte de los países europeos mantienen la tradición de aceptar que descendientes de bisabuelos y tatarabuelos de dicha nacionalidad efectúen los trámites respectivos para obtener la doble nacionalidad, la Sociedad Genealógica de igual forma ofrece como función principal los servicios de manera particular, para ayudar o apoyar a los interesados o interesadas en realizar investigaciones, tramitar certificaciones de documentos, rectificaciones, legalizaciones, apostillas, traducciones al italiano de documentos civiles, eclesiásticos, entre otros, para efectuar las gestiones respectivas directamente en tal país, con el fin de obtener la doble nacionalidad.</p>
-    						</div>
-    					</div>
-    				</div>
-    				
-    				
-    				<div id="servicios" style="display: none;">
-    					<div class="row">
-    						<div class="col-sm-1"></div>
-    						<div class="col-sm-11">
-    							<p style="text-align: justify;">También ponen a disposición los mismos servicios a empresas, entidades, familias o personas que descienden por vía materna y desean obtener su derecho a la Nacionalidad Europea.</p>
+			<p>
+				<div id="finalidad">
+					<div class="row">
+						<div class="col-sm-1"></div>
+						<div class="col-sm-11">
+							<p style="text-align: justify;">Ya que gran parte de los países europeos mantienen la tradición de aceptar que descendientes de bisabuelos y tatarabuelos de dicha nacionalidad efectúen los trámites respectivos para obtener la doble nacionalidad, la Sociedad Genealógica de igual forma ofrece como función principal los servicios de manera particular, para ayudar o apoyar a los interesados o interesadas en realizar investigaciones, tramitar certificaciones de documentos, rectificaciones, legalizaciones, apostillas, traducciones al italiano de documentos civiles, eclesiásticos, entre otros, para efectuar las gestiones respectivas directamente en tal país, con el fin de obtener la doble nacionalidad.</p>
+						</div>
+					</div>
+				</div>
+				
+				
+				<div id="servicios" style="display: none;">
+					<div class="row">
+						<div class="col-sm-1"></div>
+						<div class="col-sm-11">
+							<p style="text-align: justify;">También ponen a disposición los mismos servicios a empresas, entidades, familias o personas que descienden por vía materna y desean obtener su derecho a la Nacionalidad Europea.</p>
 
-    							<p style="text-align: justify;">Ofrecen de manera particular los servicios para efectuar investigaciones y construir árboles genealógicos para interesados particulares en conocer sus raíces ancestrales.</p>
-    						</div>
-    					</div>
-    				</div>
+							<p style="text-align: justify;">Ofrecen de manera particular los servicios para efectuar investigaciones y construir árboles genealógicos para interesados particulares en conocer sus raíces ancestrales.</p>
+						</div>
+					</div>
+				</div>
 
-    				<div id="apoyanos" style="display: none;">
-    					<div class="row">
-    						<div class="col-sm-1"></div>
-    						<div class="col-sm-11">
-    							<p style="text-align: justify;">La Sociedad Genealógica "El León de la Cordillera", expresa que: considerando que las investigaciones genealógicas y tramitaciones de documentos certificados son costosos, invitan a instituciones y a particulares que estén interesados en apoyar con aportes de recursos materiales o financieros para fortalecer y ampliar los esfuerzos y metas que se han trazado en la formación de un árbol multifamiliar que hasta la presente fecha se acerca a las cincuenta mil personas.</p>
-    						</div>
-    					</div>
-    				</div>
-    			</p>
-    		</div>
-    	</div>
-    </div>
+				<div id="apoyanos" style="display: none;">
+					<div class="row">
+						<div class="col-sm-1"></div>
+						<div class="col-sm-11">
+							<p style="text-align: justify;">La Sociedad Genealógica "El León de la Cordillera", expresa que: considerando que las investigaciones genealógicas y tramitaciones de documentos certificados son costosos, invitan a instituciones y a particulares que estén interesados en apoyar con aportes de recursos materiales o financieros para fortalecer y ampliar los esfuerzos y metas que se han trazado en la formación de un árbol multifamiliar que hasta la presente fecha se acerca a las cincuenta mil personas.</p>
+						</div>
+					</div>
+				</div>
+			</p>
+		</div>
+	</div>
 
 <?php
 	require_once "footer.php";
