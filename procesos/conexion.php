44dc34a473c792ff0a4141f89af1150b5f226db8
ErickBackDev <erickson17graterol@gmail.com>
2024-10-07 23:00:40 -0400

    Modify connection to work differently in production and development

--- procesos/conexion.php
+++ procesos/conexion.php
@@ -1,21 +1,36 @@
 <?php
 
-	require_once dirname(__DIR__) . '/vendor/autoload.php'; // Import autolad absolutely
-	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-	$dotenv->load();
+require_once dirname(__DIR__) . '/vendor/autoload.php'; // Importar autoload absolutamente
 
-	// Connect to the database using environment variables
-	$conectar = mysqli_connect(
-		$_ENV['DB_HOST'],
-		$_ENV['DB_USER'],
-		$_ENV['DB_PASS'],
-		$_ENV['DB_NAME']
-	);
+// Cargar las variables de entorno solo en desarrollo
+if (getenv('APP_ENV') === 'development') {
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__)); // Carga desde la raíz
+    $dotenv->load();
+}
 
-	$conectar->set_charset('utf8mb4'); // Make DB recognize special characters
+// Conectar a la base de datos usando variables de entorno
+if (getenv('APP_ENV') === 'production') {
+    // Conexión en producción
+    $conectar = mysqli_connect(
+        getenv('DB_HOST'),
+        getenv('DB_USER'),
+        getenv('DB_PASS'),
+        getenv('DB_NAME')
+    );
+} else {
+    // Conexión en desarrollo
+    $conectar = mysqli_connect(
+        $_ENV['DB_HOST'],
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASS'],
+        $_ENV['DB_NAME']
+    );
+}
 
-	if(!$conectar){
-		die("No se Pudo Conectar con el Servidor: " . mysqli_connect_error());
-	}
+$conectar->set_charset('utf8mb4'); // Hacer que la base de datos reconozca caracteres especiales
+
+if (!$conectar) {
+    die("No se Pudo Conectar con el Servidor: " . mysqli_connect_error());
+}
 
 ?>
